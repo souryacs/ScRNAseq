3c750b5c57ccabec0d61602caad3fe8df01c9bec
Sourya Bhattacharyya <souryacs@users.noreply.github.com>
2023-01-18 23:55:53 -0800

    Add files via upload

--- FeaturePlot/Featureplot.R
+++ FeaturePlot/Featureplot.R
@@ -1,1 +1,24 @@
+library(Seurat)
+library(ggplot2)
+
+args <- commandArgs(trailingOnly = TRUE)
+SeuratObjFile <- args[1]
+GeneListFile <- args[2]
+Outplotfile <- args[3]
+
+system(paste("mkdir -p", dirname(Outplotfile)))
+
+# read Seurat object
+sc.obj <- readRDS(SeuratObjFile)
+DefaultAssay(sc.obj) <- 'RNA'
+
+# read input gene list file
+GeneListData <- read.table(GeneListFile, header=F, sep="\t", stringsAsFactors=F) 
+
+GeneList <- GeneListData[,1]
+cat(sprintf("\n Input gene list - number of genes : %s  gene list : %s ", length(GeneList), paste(GeneList, collapse=" ")))
+
+pdf(Outplotfile, width = 8, height = 20)
+print(FeaturePlot(object = sc.obj, features = intersect(GeneList, rownames(sc.obj)), reduction = "umap", cols = c("grey", "blue"), ncol = 3, label = TRUE, label.size = 1, pt.size = 0.1, repel = TRUE))
+dev.off()
 
---
+++ FeaturePlot/README.md
@@ -0,0 +1,25 @@
+Seurat Featureplot
+==================
+
+	Rscript Featureplot.R *SeuratObjFile* *GeneListFile* *OutPlotFile*
+
+	where,
+
+		*SeuratObjFile*: Seurat object file (in .rds format)
+
+		*GeneListFile*: File containing the target genes for plotting (one gene per row - assuming the file has no header)
+
+		*OutPlotFile*: File to contain the output featureplot.
+
+	*Note: User can check the Seurat function FeaturePlot (https://satijalab.org/seurat/reference/featureplot) for reference.
+
+
+Queries
+=======
+
+For any query, please e-mail:
+Sourya Bhattacharyya
+
+sourya@lji.org
+
+
