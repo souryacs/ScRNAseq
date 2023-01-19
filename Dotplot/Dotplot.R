6002de4a50d26fcdffaf7197b43bdef36901d50d
Sourya Bhattacharyya <souryacs@users.noreply.github.com>
2023-01-18 23:55:21 -0800

    Add files via upload

--- Dotplot/Dotplot.R
+++ Dotplot/Dotplot.R
@@ -1,1 +1,16 @@
+library(Seurat)
+library(ggplot2)
 
+args <- commandArgs(trailingOnly = TRUE)
+SeuratObjFile <- args[1]
+GeneListFile <- args[2]
+OutPlotFile <- args[3]
+
+sc.obj <- readRDS(SeuratObjFile)
+
+GeneListData <- read.table(GeneListFile, header=F, sep="\t", stringsAsFactors=F)
+GeneList <- GeneListData[,1]
+cat(sprintf("\n Input gene list - number of genes : %s  gene list : %s ", length(GeneList), paste(GeneList, collapse=" ")))
+
+p <- DotPlot(object = sc.obj, features = GeneList, assay="RNA") + scale_colour_gradient2(low="royalblue3", mid="khaki1", high="tomato3") + theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+ggsave(filename=OutPlotFile, plot=p, width=12, height=6)
---
+++ Dotplot/README.md
@@ -0,0 +1,23 @@
+Seurat Dotplot
+==================
+
+	Rscript Dotplot.R *SeuratObjFile* *GeneListFile* *OutPlotFile*
+
+	where,
+
+		*SeuratObjFile*: Seurat object file (in .rds format)
+
+		*GeneListFile*: File containing the target genes for plotting (one gene per row - assuming the file has no header)
+
+		*OutPlotFile*: File to contain the output dotplot.
+
+	*Note: User can check the Seurat function DotPlot (https://satijalab.org/seurat/reference/dotplot) for reference.
+
+
+Queries
+=======
+
+For any query, please e-mail:
+Sourya Bhattacharyya
+
+sourya@lji.org
